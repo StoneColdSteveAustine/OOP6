42973a618997eff0833d7649fa2c1e2f1863f513
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2023-07-28 20:11:56 +0600

    Create SumCalculator.java

---
+++ SRP/SumCalculator.java
@@ -0,0 +1,13 @@
+package SRP;
+
+import java.util.List;
+
+public class SumCalculator {
+    public double sum(List<? extends Number> numbers) {
+        double res = 0.0;
+        for (Number n : numbers) {
+            res += n.doubleValue();
+        }
+        return res;
+    }
+}
